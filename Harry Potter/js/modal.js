f32f4564c49abc931022653df46262fa1b8b540c
darlstll <125458253+darlstll04@users.noreply.github.com>
2023-06-17 23:33:23 +0500

    Add files via upload

--- Harry Potter/js/modal.js
+++ Harry Potter/js/modal.js
@@ -11,14 +11,14 @@ const closeModal = () => {
   overlay.classList.remove("show");
 };
 
+
 export const createModal = (characters = null, data = null) => {
   characters.forEach((character) => {
-    character.addEventListener("click", () => {
-      const characterId = character.id
-      const {image, name, gender, house, dateOfBirth, actor, alternate_names, alternate_actors} = data.find((el) => el.id === characterId)
-      modal.innerHTML = 
-      `<div class="modal__header">
-        <img class="modal-img" src="${image}" alt="${name}"/>
+    character.addEventListener("click", () => { 
+      const characterId = character.id;
+      modal.innerHTML = ({image, name, gender, house, dateOfBirth, actor, alternate_names, alternate_actors} = data.find((el) => el.id === characterId))=>
+        `<div class="modal__header">
+        <img class="modal-img" src="${image}" alt=""/>
 		  <button class="close-modal">&times;</button>
       </div>
 
@@ -30,10 +30,10 @@ export const createModal = (characters = null, data = null) => {
         <p class="modal__body-actor"><span>Actor:</span> ${actor}</p>
         <p class="modal__body-altername"><span>Alternate names:</span> ${alternate_names}</p>
         <p class="modal__body-alteractor"><span>Alternate actors:</span> ${alternate_actors}</p>
-      </div>`;
-      showModal()
+      </div>
+      `;
+      showModal();
 
-      
       const closeModalBtn = document.querySelector(".close-modal");
       closeModalBtn.addEventListener("click", closeModal);
     });
